27ee41a1397e0849cd80d3a25b2ac5b5892188a3
clarajohnson <clarajohnson986@utexas.edu>
2018-09-19 18:36:15 -0500

    Added my part of allCodes

--- main.c
+++ main.c
@@ -12,11 +12,48 @@ TableEntry symbolTable[MAX_SYMBOLS];
 typedef struct {
     int iter;
     const char *array[8];
-    int hexop; //hey
+    int hexop;
 } OpCodes;
 OpCodes allCodes[14];
 
 int main () {
+    allCodes[0].iter = 1;
+    allCodes[0].array[0] = "ADD";
+    allCodes[0].hexop = 0x1;
+
+    allCodes[1].iter = 1;
+    allCodes[1].array[0] = "AND";
+    allCodes[1].hexop = 0x5;
+
+    allCodes[2].iter = 8;
+    allCodes[2].array[0] = "BRN";
+    allCodes[2].array[1] = "BRZ";
+    allCodes[2].array[2] = "BRP";
+    allCodes[2].array[3] = "BR";
+    allCodes[2].array[4] = "BRZP";
+    allCodes[2].array[5] = "BRNP";
+    allCodes[2].array[6] = "BRNZ";
+    allCodes[2].array[7] = "BRNZP";
+    allCodes[2].hexop = 0x0;
+
+    allCodes[3].iter = 2;
+    allCodes[3].array[0] = "JMP";
+    allCodes[3].array[1] = "RET";
+    allCodes[3].hexop = 0xC;
+
+    allCodes[4].iter = 2;
+    allCodes[4].array[0] = "JSR";
+    allCodes[4].array[1] = "JSRR";
+    allCodes[4].hexop = 0x4;
+
+    allCodes[5].iter = 1;
+    allCodes[5].array[0] = "LDB";
+    allCodes[5].hexop = 0x2;
+
+    allCodes[6].iter = 1;
+    allCodes[6].array[0] = "LDW";
+    allCodes[6].hexop = 0x6;
+
     allCodes[7].iter = 1;
     allCodes[7].array[0] = "LEA";
     allCodes[7].hexop = 0xE;
@@ -47,7 +84,6 @@ int main () {
     allCodes[13].array[0] = "XOR";
     allCodes[13].hexop = 0x9;
 
-    //why though
     char str1[20]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     FILE * fptr;
     fptr = fopen("testing.txt", "r");
